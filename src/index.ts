1840192346c52325547b4e468d8368c305b08a0b
Guilherme-Soares-Sequeira <guilhermesoaressequeira@hotmail.com>
2025-03-18 21:07:31 +0000

    small progress.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,51 @@
 import Query from "@specs-feup/lara/api/weaver/Query.js"
-import { Loop } from "@specs-feup/clava/api/Joinpoints.js"
+import { Loop, Statement, ReturnStmt, GotoStmt, Break, Continue, Type, BuiltinType, TypedefType, Expression, Decl } from "@specs-feup/clava/api/Joinpoints.js"
 import ClavaJoinPoints from "@specs-feup/clava/api/clava/ClavaJoinPoints.js"
+
+const int8_t = "int8_t";
+
+function isInt8(typed: Type | Expression | Decl): boolean {
+    let type: Type;
+
+    if (typed instanceof Type) {
+        type = typed;
+    } else {
+        if (!typed.hasType) return false;
+        type = typed.type;
+    }
+
+    if (!(type instanceof TypedefType)) return false;
+    
+    let typedefType: TypedefType = type as TypedefType;
+    do {
+        if (typedefType.decl !== null && typedefType.decl.name === int8_t) return true;
+        if (!(typedefType.underlyingType instanceof TypedefType)) return false;
+
+        typedefType = typedefType.underlyingType;
+    } while (true);
+}
+
+function isControlFlowAltering(stmt: Statement) {
+    return stmt instanceof ReturnStmt || stmt instanceof GotoStmt || stmt instanceof Break || stmt instanceof Continue;
+}
+
+function hasPredictableStep(loop: Loop): boolean {
+    if (Query.searchFrom(loop, Statement, isControlFlowAltering).get().length !== 0) {
+        return false;
+    }
+
+    // loop.
+
+    return false;
+}
+
+function hasRegularControlFlow(loop: Loop): boolean {
+    return hasPredictableStep(loop) // &&
+}
+
+function loopIsSuitable(loop: Loop): boolean {
+    return loop.kind === "for" && hasRegularControlFlow(loop) // &&
+}
 
 const forLoops = Query.search(Loop, (loop: Loop) => loop.kind === "for").get();
 
@@ -9,7 +54,7 @@ for (const forLoop of forLoops) {
     console.log("Cond: {\n" + forLoop.cond.code + "\n}\n");
     
     if (forLoop.hasCondRelation) {
-        //console.log("CondRelation: {\n" + forLoop.condRelation + "\n}\n");
+         console.log("CondRelation: {\n" + forLoop.condRelation + "\n}\n");
     }
 
     console.log("ControlVar: {\n" + forLoop.controlVar + "\n}\n");
