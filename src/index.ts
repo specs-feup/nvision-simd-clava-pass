2e4094a5b348455a3fd48d5328d795f91afaee1b
Guilherme-Soares-Sequeira <guilhermesoaressequeira@hotmail.com>
2025-06-11 14:35:55 +0100

    Searching suitable loops works.

--- src/index.ts
+++ src/index.ts
@@ -1,127 +1,44 @@
 import Query from "@specs-feup/lara/api/weaver/Query.js"
-import { Loop, Statement, ReturnStmt, GotoStmt, Break, Continue, Type, BuiltinType, TypedefType, Expression, Decl, Varref, BinaryOp, UnaryOp, Joinpoint } from "@specs-feup/clava/api/Joinpoints.js"
+import { Loop, Statement, ReturnStmt, GotoStmt, Break, Continue, Type, BuiltinType, TypedefType, Expression, Decl, Varref, BinaryOp, UnaryOp, Joinpoint, IntLiteral, Vardecl, Call, ArrayAccess, FunctionJp, Op, Body } from "@specs-feup/clava/api/Joinpoints.js"
 import ClavaJoinPoints from "@specs-feup/clava/api/clava/ClavaJoinPoints.js"
+import SimplifyAssignment from "@specs-feup/clava/api/clava/code/SimplifyAssignment.js";
+import { propagateAndFoldConstants } from "./constprop/propandfold.js";
+import { isConstantIn } from "./utils/constants.js";
+import { isVarrefOf } from "./utils/varReferences.js";
+import { unaryOp } from "specs-feup/clava/api/ClavaJoinPoints.js"
 
-const int8_t: string = "int8_t";
 
-function isInt8(typed: Type | Expression | Decl): boolean {
-    let type: Type;
-
-    if (typed instanceof Type) {
-        type = typed;
-    } else {
-        if (!typed.hasType) return false;
-        type = typed.type;
-    }
-
-    if (!(type instanceof TypedefType)) return false;
-    
-    let typedefType: TypedefType = type as TypedefType;
-    do {
-        if (typedefType.decl !== null && typedefType.decl.name === int8_t) return true;
-        if (!(typedefType.underlyingType instanceof TypedefType)) return false;
-
-        typedefType = typedefType.underlyingType;
-    } while (true);
-}
+const packingFactorAcceptedArrayTypes = new Map([
+    [2, ["__int16_t", "__uint16_t", "int16_t", "uint16_t"]],
+    [4, ["__int8_t", "__uint8_t", "int8_t", "uint8_t"]]
+]);
 
 function altersControlFlow(stmt: Statement) {
     return stmt instanceof ReturnStmt || stmt instanceof GotoStmt || stmt instanceof Break || stmt instanceof Continue;
 }
 
 /**
- * Indicates whether "examinee"'s value is known at compile time when referenceJoinpoint is being executed.
- * For example, given a "i += a" statement, we might want to know whether the value of "a" is known.
- * This can be achieved by checking if the last assignment to "a" before the statement was done with a 
- * value known at compile time (with a literal, #define, expression of literal values, etc.)
- * @param referenceJoinpoint dictates where the assignments are searched from
- * @param examinee representation of the value to be searched. Could be a literal or a variable's name
+ * Checks if the loop's step value can be parsed as an Int
  */
-function hasKnownValue(referenceJoinpoint: Joinpoint, examinee: string) {
-    // TODO: Check #defines and statically initialized (or last assigned) variables
-    return !Number.isNaN(parseInt(examinee));
+function hasKnownIntStepValue(loop: Loop): boolean {
+    return loop.stepValue !== null && loop.stepValue !== undefined && !Number.isNaN(parseInt(loop.stepValue));
 }
 
 /**
- * Checks whether the loop's step value is constant or not. This is trivial if the step is "i++",
- * but it might also be "i += a", where a was assigned "1" right before the loop.
- * See the hasKnownValue function's documentation for more info
- * @param loop 
- * @returns 
+ * Checks if the loop's end value can be parsed as an Int
  */
-function hasKnownStepValue(loop: Loop): boolean  {
-
-    return hasKnownValue(loop, loop.stepValue);
+function hasKnownEndValue(loop: Loop): boolean {
+    return loop.endValue !== null && loop.endValue !== undefined && !Number.isNaN(parseInt(loop.endValue));
 }
 
-/**
- * Checks whether the loop's end value is constant or not, which is not trivial if the end value is not
- * a literal.
- * See the hasKnownValue function's documentation for more info
- * @param loop 
- * @returns 
- */
-function hasKnownEndValue(loop: Loop): boolean  {
-    // TODO: Check #defines and statically initialized (or last assigned) variables
+function hasConstantPredictableStep(loop: Loop): boolean {
+    if (loop.controlVar === undefined || loop.controlVar === null) return false;
 
-    return hasKnownValue(loop, loop.endValue);
-}
+    if (!hasKnownIntStepValue(loop)) return false;
 
-function searchVariableAssignments(searchRoot: Joinpoint, variableName: string): BinaryOp[] {
-    return Query.searchFrom(searchRoot, BinaryOp, (op) => {
-        if (!op.isAssignment || !(op.left instanceof Varref)) return false;
+    const loopControlVarDecl: Vardecl = Query.searchFrom(loop, Varref, { name: loop.controlVar }).getFirst()!.vardecl!;
 
-        let varref: Varref = op.left as Varref;
-        return varref.name === variableName;
-    }).get();
-}
-
-function unaryOperationModifiesOperandValue(op: UnaryOp): boolean {
-    switch (op.kind) {
-        case "post_inc":
-        case "post_dec":
-        case "pre_inc":
-        case "pre_dec":
-            return true;
-    }
-    return false;
-}
-
-type VariableModification = {
-    variableName: string,
-    assignments: BinaryOp[],
-    unaryOps: UnaryOp[]
-}
-
-function searchVariableModifications(searchRoot: Joinpoint, variableName: string): VariableModification {
-    let assignments: BinaryOp[] = searchVariableAssignments(searchRoot, variableName);
-
-    let modifyingUnaryOps: UnaryOp[] = Query.searchFrom(searchRoot, UnaryOp, (op) => {
-        if (!unaryOperationModifiesOperandValue(op)) return false;
-        
-        let operand: Expression = op.operand;
-        if (!(operand instanceof Varref)) return false;
-
-        return operand.name === variableName;
-    }).get();
-    
-    return {
-        variableName: variableName,
-        assignments: assignments,
-        unaryOps: modifyingUnaryOps
-    };
-}
-
-function variableModificationsCount(searchRoot: Joinpoint, variableName: string): number {
-    let modifications = searchVariableModifications(searchRoot, variableName);
-
-    return modifications.assignments.length + modifications.unaryOps.length;
-}
-
-function hasPredictableStep(loop: Loop): boolean {
-    if (loop.controlVar === undefined) return false;
-
-    return variableModificationsCount(loop.body, loop.controlVar) === 0;
+    return isConstantIn(loopControlVarDecl, loop.body);
 }
 
 function endValueIsConstant(loop: Loop): boolean {
@@ -130,75 +47,105 @@ function endValueIsConstant(loop: Loop): boolean {
     if (endValue === undefined || endValue === null) return true;
 
     let endValueIsLiteral: boolean = !Number.isNaN(parseInt(endValue));
-    
+
     if (endValueIsLiteral) return true;
 
-    // therefore endValue is a variable name
-
-    return variableModificationsCount(loop.body, endValue) === 0;
+    return false;
 }
 
 function hasRegularControlFlow(loop: Loop): boolean {
-    let hasNoCustomControlFlow: boolean = Query.searchFrom(loop, Statement, altersControlFlow).get().length !== 0;
+    let hasNoCustomControlFlow: boolean = Query.searchFrom(loop, Statement, altersControlFlow).get().length === 0;
 
-    return hasNoCustomControlFlow && hasPredictableStep(loop) && endValueIsConstant(loop);
+    return hasNoCustomControlFlow && hasConstantPredictableStep(loop) && endValueIsConstant(loop);
 }
 
-function tryComputeIntegerValue(referencePoint: Joinpoint, value: string): number {
-    // TODO: Check what is returned from loop.stepValue is it's something like i += a+2 or i += a*2
-    // TODO: Check other options, see hasKnownValue for deets
+function isInsideMultiplication(jp: Joinpoint): boolean {
+    let parent: Joinpoint = jp.parent;
+    while (parent instanceof Op) {
+        if (parent instanceof BinaryOp && parent.kind === "mul") return true;
+        parent = parent.parent;
+    };
 
-    let tryInt = parseInt(value);
-
-    if (Number.isNaN(tryInt)) {
-        throw new Error("value is not an integer literal... other options are currently unimplemented");
-    }
-
-    return tryInt;
-}
-
-function stepHasKnownNumberValue(loop: Loop, expectedValue: number) {
-    if (!hasKnownStepValue(loop)) return false;
-    let actualStepValue: number;
-
-    try {
-        actualStepValue = tryComputeIntegerValue(loop, loop.stepValue);
-    } catch (e) {
-        return false;
-    }
-
-    return expectedValue === actualStepValue;
-}
-
-function endValueIsDivisible(loop: Loop, diviser: number): boolean {
-    if (!hasKnownEndValue(loop)) return false;
-    
-    let endValue: number;
-
-    try {
-        endValue = tryComputeIntegerValue(loop, loop.endValue);
-    } catch (e) {
-        return false;
-    }
-
-    return endValue % diviser === 0;
-
+    return false;
 }
 
 function loopIsSuitable(loop: Loop, packingFactor: number): boolean {
-    // TODO: In the future, end value might not need to be divisible, similar to loop unrolling
 
-    // packingFactor*2 since nvision's hw performs two packed simd multiplications sequencially (a*b and c*d, where each letter are actually packed values)
-    return loop.kind === "for" && hasRegularControlFlow(loop) && stepHasKnownNumberValue(loop, 1) && endValueIsDivisible(loop, packingFactor*2);
+    if (!(packingFactorAcceptedArrayTypes.has(packingFactor))) {
+        throw new Error(`Tried to use unsupported packingFactor: ${packingFactor}`);
+    }
+
+    if (!(loop.kind === "for" && hasRegularControlFlow(loop) && parseInt(loop.initValue) === 0) && parseInt(loop.stepValue) === 1) {
+        return false;
+    }
+
+    const declaredVariablesInLoop: Vardecl[] = Query.searchFrom(loop, Vardecl).get();
+    const externalVariableModifications: Varref[] = Query.searchFrom(loop, Varref, varref => {
+        if (!(varref.use === "write" || varref.use === "readwrite")) return false;
+
+        if (varref.vardecl === undefined || varref.vardecl === null) return false;
+        return declaredVariablesInLoop.filter(vardecl => vardecl.equals(varref.vardecl)).length === 0;
+    }).get();
+
+    const calls: Call[] = Query.searchFrom(loop, Call).get();
+
+    if (calls.length !== 0 || externalVariableModifications.length !== 1) {
+        return false;
+    }
+
+
+    const accumVarref: Varref = externalVariableModifications[0];
+
+    // for now, the accumulation should be of the type accum = accum + A[...] * B[...]
+    if (!(accumVarref.parent instanceof BinaryOp)) return false;
+    const accumAssignment: BinaryOp = accumVarref.parent as BinaryOp;
+    if (accumAssignment.kind !== "assign" || !accumAssignment.left.equals(accumVarref)) return false;
+
+    if (!(accumAssignment.right instanceof BinaryOp)) return false;
+    const assignmentRightJp: BinaryOp = accumAssignment.right as BinaryOp;
+    if (assignmentRightJp.kind !== "add" || !isVarrefOf(assignmentRightJp.left, accumVarref.vardecl) || !(assignmentRightJp.right instanceof BinaryOp))
+        return false;
+
+    const multiplicationOfArrayAccesses: BinaryOp = assignmentRightJp.right;
+    if (multiplicationOfArrayAccesses.kind !== "mul" || !(multiplicationOfArrayAccesses.left instanceof ArrayAccess) || !(multiplicationOfArrayAccesses.right instanceof ArrayAccess))
+        return false;
+
+    if (!packingFactorAcceptedArrayTypes.get(packingFactor)?.includes(multiplicationOfArrayAccesses.left.type.desugar.code)) return false;
+    if (!packingFactorAcceptedArrayTypes.get(packingFactor)?.includes(multiplicationOfArrayAccesses.right.type.desugar.code)) return false;
+
+    // does not work if control var is initialized outside of loop...
+    const controlVal: Vardecl = Query.searchFrom(loop, Vardecl, { name: loop.controlVar }).getFirst()!;
+
+    if (Query.searchFrom(multiplicationOfArrayAccesses.left, Varref, varref => isVarrefOf(varref, controlVal) && !isInsideMultiplication(varref)).get().length === 0) return false;
+    if (Query.searchFrom(multiplicationOfArrayAccesses.right, Varref, varref => isVarrefOf(varref, controlVal) && !isInsideMultiplication(varref)).get().length === 0) return false;
+
+    return true;
 }
 
+function applyTransformation(suitableForLoop: Loop): void {
+    const accumVarref: Varref = Query.searchFrom(suitableForLoop).search(Body).search(BinaryOp, { kind: "assign" }).search(Varref, { use: "write" }).getFirst()!;
+    const arrayAccesses: ArrayAccess[] = Query.searchFrom(accumVarref.parent, ArrayAccess).get();
+
+    if (arrayAccesses.length !== 1 && arrayAccesses.length !== 2) throw new Error("Unsupported number of array accesses");
+
+    const arrayA: Varref = arrayAccesses[0].arrayVar as Varref;
+    const arrayB: Varref = (arrayAccesses.length === 1 ? arrayAccesses[0] : arrayAccesses[1]).arrayVar as Varref;
+    const endValue = suitableForLoop.endValue;
+}
+
+Query.search(Loop).search(Body).search(BinaryOp, binOp => binOp.isAssignment).get().map(SimplifyAssignment);
+propagateAndFoldConstants();
 /*
     I am first considering the case where the original value is an int8_t, since
     nvision's instructions use 32 bit packed values, the packing factor is 4x!
 */
-const packingFactor = 4; 
+const packingFactor = 4;
 
 const forLoops = Query.search(Loop, (loop: Loop) => loopIsSuitable(loop, packingFactor)).get();
+
+for (const suitableLoop of forLoops) {
+    applyTransformation(suitableLoop);
+}
 
 console.log("Found " + forLoops.length + " forloops.")
 
--- src/input/forloop.c
+++ src/input/forloop.c
@@ -1,18 +1,128 @@
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 
+int32_t accum = 0;
+
+void mac_sim_8b(int32_t a, int32_t b, int32_t c, int32_t d) {
+  int8_t *a_cast = (int8_t *)(&a);
+  int8_t *b_cast = (int8_t *)(&b);
+  int8_t *c_cast = (int8_t *)(&c);
+  int8_t *d_cast = (int8_t *)(&d);
+
+  accum += a_cast[0] * b_cast[0];
+  accum += a_cast[1] * b_cast[1];
+  accum += a_cast[2] * b_cast[2];
+  accum += a_cast[3] * b_cast[3];
+
+  accum += c_cast[0] * d_cast[0];
+  accum += c_cast[1] * d_cast[1];
+  accum += c_cast[2] * d_cast[2];
+  accum += c_cast[3] * d_cast[3];
+}
+
+void mac_sim_16b(int32_t a, int32_t b, int32_t c, int32_t d) {
+  int16_t *a_cast = (int16_t *)(&a);
+  int16_t *b_cast = (int16_t *)(&b);
+  int16_t *c_cast = (int16_t *)(&c);
+  int16_t *d_cast = (int16_t *)(&d);
+
+  accum += a_cast[0] * b_cast[0];
+  accum += a_cast[1] * b_cast[1];
+
+  accum += c_cast[0] * d_cast[0];
+  accum += c_cast[1] * d_cast[1];
+}
+
+int32_t clear_read_sim() {
+  int32_t temp = accum;
+  accum = 0;
+
+  return temp;
+}
+
+/*
+    0 1 -> corre uma matrix_col e sai
+    0 1 2 -> corre uma matrix_col, corre a tail
+    0 1 2 3 4 -> corre 2 matrix_col, corre a tail
+*/
+void nvision_matrix_col_8b(int32_t *A, int32_t *B, int32_t *accum,
+                           size_t length) {
+  clear_read_sim();
+
+  for (int i = 0; i < length - 1; i += 2) {
+    mac_sim_8b(A[i], B[i], A[i + 1], B[i + 1]);
+  }
+
+  if (length % 2 == 1) {
+    int8_t *a_cast = (int8_t *)&(A[length - 1]);
+    int8_t *b_cast = (int8_t *)&(B[length - 1]);
+
+    accum += a_cast[0] * b_cast[0];
+    accum += a_cast[1] * b_cast[1];
+    accum += a_cast[2] * b_cast[2];
+    accum += a_cast[3] * b_cast[3];
+  }
+
+  *accum += clear_read_sim();
+}
+
+void nvision_matrix_col_8b_alt(int8_t *A, int8_t *B, int32_t *accum,
+                               size_t length) {
+  clear_read_sim();
+
+  int mac_len = length / 4;
+  int32_t *A_cast = (int32_t *)A;
+  int32_t *B_cast = (int32_t *)B;
+
+  for (int i = 0; i < mac_len - 1; i += 2) {
+    mac_sim_8b(A_cast[i], B_cast[i], A_cast[i + 1], B_cast[i + 1]);
+  }
+
+  if (length % 2 == 1) {
+    int8_t *a_cast = (int8_t *)&(A[length - 1]);
+    int8_t *b_cast = (int8_t *)&(B[length - 1]);
+
+    accum += a_cast[0] * b_cast[0];
+    accum += a_cast[1] * b_cast[1];
+    accum += a_cast[2] * b_cast[2];
+    accum += a_cast[3] * b_cast[3];
+  }
+
+  *accum += clear_read_sim();
+}
+
 int main(void) {
-    int32_t result = 0;
-    int8_t vector[] = {10, 4, 8, 2, 6, 12};
-    int8_t matrix_col[] = {10, 4, 8, 2, 6, 12};
+  int32_t result = 0;
+  size_t len = 8; // len has to be multiple of 8
+  int8_t vector[8] = {10, 4, 8, 2, 6, 12, 3, 7};
+  int8_t matrix_col[8] = {10, 4, 8, 2, 6, 12, 5, 1};
 
-    size_t len = 6;
-    
-    for (size_t i = 0; i < len; i++) {
-        result += vector[i] * matrix_col[i];
-    }
+  // ok
+  for (size_t i = 0; i < len; i++) {
+    result += vector[i] * matrix_col[i];
+  }
 
-    printf("%d\n", result);
-    return 0;
+  // ok
+  for (size_t i = 0; i < len; i++) {
+    result = result + vector[i] * matrix_col[i];
+  }
+
+  nvision_matrix_col_8b((int32_t *)matrix_col, (int32_t *)vector, &result,
+                        len / 8);
+
+  // problem
+  for (size_t i = 0; i < len; i++) {
+    int32_t temp = vector[i] * matrix_col[i];
+    result += temp;
+  }
+
+  // problem
+  for (size_t i = 0; i < len; i++) {
+    int32_t temp = vector[i] * matrix_col[i];
+    result += temp;
+  }
+
+  printf("%d\n", result);
+  return 0;
 }
