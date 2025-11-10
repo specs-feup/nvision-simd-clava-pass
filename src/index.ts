d666dbfa5298a6381f14f7736610fee2c108eaa8
Guilherme-Soares-Sequeira <guilhermesoaressequeira@hotmail.com>
2025-11-10 19:49:18 +0000

    still working it out...

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,54 @@
+#!/usr/bin/env node
+
+import process from "node:process";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import Clava from "@specs-feup/clava/api/clava/Clava.js";
 import { applyPass } from "./pass.js";
 
-applyPass(true, false);
+import WeaverLauncher from "@specs-feup/lara/code/WeaverLauncher.js";
+import { weaverConfig, printstuff } from "./myWeaverConfig.js";
+
+printstuff();
+
+const weaverLauncher = new WeaverLauncher(weaverConfig);
+await weaverLauncher.execute();
+
+const argv = yargs(hideBin(process.argv))
+    .options({
+        verbose: {
+            type: 'boolean',
+            describe: "hi",
+            alias: 'v',
+            default: false
+        },
+        'use-sw-sim': {
+            type: 'boolean',
+            describe: "something",
+            default: false
+        },
+        'input': {
+            type: 'string',
+            alias: 'i',
+            required: true,
+            describe: "A Clava input string. "
+        },
+        'output-path': {
+            type: 'string',
+            alias: 'o',
+            required: true,
+            normalize: true,
+            default: "woven_code",
+            describe: "Directory where the output should be written"
+        }
+    })
+    .parseSync();
+
+// Clava.runClava(['-p', argv.input]);
+console.log(`verbose: ${argv.verbose}`);
+console.log(`use-sw-sim: ${argv.useSwSim}`);
+console.log(`: ${argv.outputPath}`);
+console.log(`input file(s): ${argv.input}`);
+
+// applyPass(true, false);
+Clava.writeCode(argv.outputPath);
